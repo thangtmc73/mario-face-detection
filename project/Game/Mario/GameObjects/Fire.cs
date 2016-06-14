78e3a6a599325f7a2fb00a1a79c336beb843c626
DuyPQ2610 <14521161duyduy@gmail.com>
2016-06-14 22:56:51 +0700

    them
    ...

---
+++ project/Game/Mario/GameObjects/Fire.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mario.GameObjects
+{
+    class Fire : BaseObject
+    {
+        public Fire(string a_name,System.Drawing.Point a_pos):base (a_name)
+        {
+            _sprite.ImageSpr.Image = global::Mario.Properties.Resources.sprFire;
+            _sprite.Position = a_pos;
+        }
+        public override void Move()
+        {
+            
+            //_sprite.Position = new System.Drawing.Point(_sprite.Position.X + 5, _sprite.Position.Y);
+        }
+        public void MoveRight()
+        {
+            _sprite.Position = new System.Drawing.Point(_sprite.Position.X + 5, _sprite.Position.Y);
+        }
+        public void MoveLeft()
+        {
+            _sprite.Position = new System.Drawing.Point(_sprite.Position.X - 5, _sprite.Position.Y);
+        }
+
+        public void CollideMonster(Monster a_monster)
+        {
+            if(CheckCollision(a_monster._sprite.ImageSpr))
+            {
+                this._sprite.Position = new System.Drawing.Point(this._sprite.Position.X, this._sprite.Position.Y + 150);
+                a_monster._monsterLife--;
+                if (a_monster._monsterLife == 0)
+                    a_monster.Die();
+                
+            }
+        }
+    }
+}
