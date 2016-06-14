54962a458ee8712b1d7dcd724fe6bcbe862fe160
DuyPQ2610 <14521161duyduy@gmail.com>
2016-06-14 22:52:57 +0700

    Duy up phan cua Duy
    Done

---
+++ project/Game/Mario/Mushroom.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mario.GameObjects
+{
+	class Mushroom : BaseObject
+	{
+        public int _type;
+
+        public Mushroom(string a_name, System.Drawing.Point a_pos) : base(a_name)
+		{
+			//_sprite.ImageSpr.Image = global::Mario.Properties.Resources.;
+			_sprite.Position = a_pos;
+		}
+		public override void Move() { }
+	}
+}
---
+++ project/Game/Mario/RedMushroom.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mario.GameObjects
+{
+	class RedMushroom : Mushroom
+	{
+		public RedMushroom(string a_name, System.Drawing.Point a_pos) : base(a_name, a_pos)
+		{
+			_sprite.ImageSpr.Image = global::Mario.Properties.Resources.sprRedMushroom;
+            _sprite.Position = a_pos;
+            base._type = 1;
+		}
+		public override void Move() { }
+	}
+}
